eaad1216be35047a7daf1948ea035989eb1765df
RicardoBatistaJr <ricardowalker01.rw@gmail.com>
2023-11-23 17:42:05 -0300

    Creating Project

---
+++ src/test/java/com/ricardo/PaymentSystemAPI/PaymentSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.PaymentSystemAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
